c3b89c5f0c973736aa253e4ec9c61d0eb7b44963
rajat-bhatt-au9 <rajatbhatt500@gmail.com>
2021-03-10 11:48:22 +0530

    created register endpoint and introduced jwt

--- server/server.js
+++ server/server.js
@@ -8,7 +8,14 @@ app.use(cors());
 //connect to DB
 connectDB();
 
+//intialize middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API RUNNING..."));
+
+//Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.PORT || 5000;
 
