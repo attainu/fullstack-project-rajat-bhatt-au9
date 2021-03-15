a37ad653aa922f06fe4fd3f969a26cef99fc439b
rajat-bhatt-au9 <rajatbhatt500@gmail.com>
2021-03-15 21:42:54 +0530

    ticket count updated and class changes to className

--- client/src/components/dashboard/Profile.js
+++ client/src/components/dashboard/Profile.js
@@ -3,54 +3,54 @@ import "./profile.css";
 const Profile = ({ user }) => {
   return (
     <div>
-      <div class='page-content page-container' id='page-content'>
-        <div class='padding'>
-          <div class='row container d-flex justify-content-center'>
-            <div class='col-xl-12 col-md-12'>
-              <div class='card user-card-full'>
-                <div class='row m-l-0 m-r-0'>
-                  <div class='col-sm-4 bg-c-lite-green user-profile'>
-                    <div class='card-block text-center text-white'>
-                      <div class='m-b-25'>
+      <div className='page-content page-container' id='page-content'>
+        <div className='padding'>
+          <div className='row container d-flex justify-content-center'>
+            <div className='col-xl-12 col-md-12'>
+              <div className='card user-card-full'>
+                <div className='row m-l-0 m-r-0'>
+                  <div className='col-sm-4 bg-c-lite-green user-profile'>
+                    <div className='card-block text-center text-white'>
+                      <div className='m-b-25'>
                         {" "}
                         <img
                           src={user.avatar}
-                          class='img-radius'
+                          className='img-radius'
                           alt='User-Profile-Image'
                         />{" "}
                       </div>
-                      <h6 class='f-w-600'>{user.name}</h6>
+                      <h6 className='f-w-600'>{user.name}</h6>
 
-                      <i class=' mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16'></i>
+                      <i className=' mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16'></i>
                     </div>
                   </div>
-                  <div class='col-sm-8'>
-                    <div class='card-block'>
-                      <h2 class='m-b-20 p-b-5 b-b-default f-w-600'>
+                  <div className='col-sm-8'>
+                    <div className='card-block'>
+                      <h2 className='m-b-20 p-b-5 b-b-default f-w-600'>
                         Welcome, {user.name}
                       </h2>
-                      <div class='row'>
-                        <div class='col-sm-6'>
-                          <p class='m-b-10 f-w-600'>Email</p>
-                          <h6 class='text-muted f-w-400'>{user.email}</h6>
+                      <div className='row'>
+                        <div className='col-sm-6'>
+                          <p className='m-b-10 f-w-600'>Email</p>
+                          <h6 className='text-muted f-w-400'>{user.email}</h6>
                         </div>
-                        <div class='col-sm-6'>
-                          <p class='m-b-10 f-w-600'>Unique Id</p>
-                          <h6 class='text-muted f-w-400'>{user._id}</h6>
-                        </div>
-                      </div>
-                      <h6 class='m-b-20 m-t-40 p-b-5 b-b-default f-w-600'></h6>
-                      <div class='row'>
-                        <div class='col-sm-6'>
-                          <p class='m-b-10 f-w-600'>Role</p>
-                          <h6 class='text-muted f-w-400'>{user.role}</h6>
-                        </div>
-                        <div class='col-sm-6'>
-                          <p class='m-b-10 f-w-600'>Active From</p>
-                          <h6 class='text-muted f-w-400'>{user.date}</h6>
+                        <div className='col-sm-6'>
+                          <p className='m-b-10 f-w-600'>Unique Id</p>
+                          <h6 className='text-muted f-w-400'>{user._id}</h6>
                         </div>
                       </div>
-                      <ul class='social-link list-unstyled m-t-40 m-b-10'>
+                      <h6 className='m-b-20 m-t-40 p-b-5 b-b-default f-w-600'></h6>
+                      <div className='row'>
+                        <div className='col-sm-6'>
+                          <p className='m-b-10 f-w-600'>Role</p>
+                          <h6 className='text-muted f-w-400'>{user.role}</h6>
+                        </div>
+                        <div className='col-sm-6'>
+                          <p className='m-b-10 f-w-600'>Active From</p>
+                          <h6 className='text-muted f-w-400'>{user.date}</h6>
+                        </div>
+                      </div>
+                      <ul className='social-link list-unstyled m-t-40 m-b-10'>
                         <li>
                           <a
                             href='#!'
@@ -61,7 +61,7 @@ const Profile = ({ user }) => {
                             data-abc='true'
                           >
                             <i
-                              class='mdi mdi-facebook feather icon-facebook facebook'
+                              className='mdi mdi-facebook feather icon-facebook facebook'
                               aria-hidden='true'
                             ></i>
                           </a>
@@ -76,7 +76,7 @@ const Profile = ({ user }) => {
                             data-abc='true'
                           >
                             <i
-                              class='mdi mdi-twitter feather icon-twitter twitter'
+                              className='mdi mdi-twitter feather icon-twitter twitter'
                               aria-hidden='true'
                             ></i>
                           </a>
@@ -91,7 +91,7 @@ const Profile = ({ user }) => {
                             data-abc='true'
                           >
                             <i
-                              class='mdi mdi-instagram feather icon-instagram instagram'
+                              className='mdi mdi-instagram feather icon-instagram instagram'
                               aria-hidden='true'
                             ></i>
                           </a>
--- client/src/components/ticket/TicketPage.js
+++ client/src/components/ticket/TicketPage.js
@@ -1,122 +1,124 @@
-import React, {useEffect} from 'react'
-import {connect} from 'react-redux'
-import {getTickets} from '../../actions/ticket'
-import {Link} from 'react-router-dom'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getTickets } from "../../actions/ticket";
+import { Link } from "react-router-dom";
 import Footer from "../layout/Footer";
 import Navbar from "../layout/Navbar";
-import TicketList from './TicketList';
+import TicketList from "./TicketList";
 
- const TicketPage = ({getTickets, ticket:{tickets, loading}}) => {
-
+const TicketPage = ({ getTickets, ticket: { tickets, loading } }) => {
   useEffect(() => {
     getTickets();
   }, [getTickets]);
 
   return (
-        <div id='wrapper'>
-    <div className='overlay'></div>
+    <div id='wrapper'>
+      <div className='overlay'></div>
 
-    <div id='page-content-wrapper'>
-      <div id='content'>
-        <div className='container-fluid p-0 px-lg-0 px-md-0'>
-          {/* Navbar */}
-          <Navbar />
+      <div id='page-content-wrapper'>
+        <div id='content'>
+          <div className='container-fluid p-0 px-lg-0 px-md-0'>
+            {/* Navbar */}
+            <Navbar />
 
-          <div className='container-fluid px-lg-4'>
-            <div className='row'>
-              <div className='col-md-12 mt-lg-4 mt-4'>
-                <div className='d-sm-flex align-items-center justify-content-between mb-4'>
-                  <h1 className='h3 mb-0 text-gray-800'> Tickets </h1>
-                  <Link to="#"  className='d-sm-inline-block btn btn-sm btn-primary shadow-sm'>
-                    <i className='fas fa-download fa-sm text-white-50'></i>
-                    Create New Ticket
-                  </Link>
-                
-                </div>
-              </div>
-              {/* vhjgjhd */}
-              <div className='col-md-12'>
-                <div className='row'>
-                  <div className='col-sm-3'>
-                    <div className='card'>
-                      <div className='card-body'>
-                        <h5 className='card-title mb-4'> Total Tickets Count </h5>
-                        <h1 className='display-5 mt-1 mb-3'>52</h1>
-                        <div className='mb-1'>
-                          {/* <span className="text-danger"> <a href="#"></a><i className="mdi mdi-arrow-bottom-right">Go to Client</i></a> </span>
-                                          <span className="text-muted">Since last week</span> */}
-                          <span className='text-danger'>
-                            <i className='mdi mdi-arrow-bottom-right'>
-                              Go to Tickets
-                            </i>
-                          </span>
-                        </div>
-                      </div>
-                    </div>
+            <div className='container-fluid px-lg-4'>
+              <div className='row'>
+                <div className='col-md-12 mt-lg-4 mt-4'>
+                  <div className='d-sm-flex align-items-center justify-content-between mb-4'>
+                    <h1 className='h3 mb-0 text-gray-800'> Tickets </h1>
+                    <Link
+                      to='#'
+                      className='d-sm-inline-block btn btn-sm btn-primary shadow-sm'
+                    >
+                      <i className='fas fa-download fa-sm text-white-50'></i>
+                      Create New Ticket
+                    </Link>
                   </div>
                 </div>
-
-  
-
-                <div className='col-md-12 mt-4'>
-                  <div className='card'>
-                    <div className='card-body'>
-                      <div className='d-md-flex align-items-center'>
-                        <div>
-                          <h4 className='card-title'>List Of Tickets</h4>
-                        </div>
-                        <div className='ml-auto'>
-                          <div className='dl'>
-                            <select className='custom-select'>
-                              <option value='0' selected=''>
-                                Monthly
-                              </option>
-                              <option value='1'>Daily</option>
-                              <option value='2'>Weekly</option>
-                              <option value='3'>Yearly</option>
-                            </select>
+                {/* vhjgjhd */}
+                <div className='col-md-12'>
+                  <div className='row'>
+                    <div className='col-sm-3'>
+                      <div className='card'>
+                        <div className='card-body'>
+                          <h5 className='card-title mb-4'>
+                            {" "}
+                            Total Tickets Count{" "}
+                          </h5>
+                          <h1 className='display-5 mt-1 mb-3'>
+                            {Object.keys(tickets).length}
+                          </h1>
+                          <div className='mb-1'>
+                            {/* <span className="text-danger"> <a href="#"></a><i className="mdi mdi-arrow-bottom-right">Go to Client</i></a> </span>
+                                          <span className="text-muted">Since last week</span> */}
+                            <span className='text-danger'>
+                              <i className='mdi mdi-arrow-bottom-right'>
+                                Go to Tickets
+                              </i>
+                            </span>
                           </div>
                         </div>
                       </div>
                     </div>
-                    <div className='table-responsive'>
-                      <table className='table v-middle'>
-                        <thead>
-                          <tr className='bg-light'>
-                          
-                            <th className='border-top-0'>Name</th>
-                            <th className='border-top-0'>Ticket Issues</th>
-                            <th className='border-top-0'>Priority</th>
-                            <th className='border-top-0'>Status</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {tickets.map((ticket)=>(
+                  </div>
 
-                            <TicketList key={ticket.id} ticket={ticket} />
-                          ))}
-                        </tbody>
-                      </table>
+                  <div className='col-md-12 mt-4'>
+                    <div className='card'>
+                      <div className='card-body'>
+                        <div className='d-md-flex align-items-center'>
+                          <div>
+                            <h4 className='card-title'>List Of Tickets</h4>
+                          </div>
+                          <div className='ml-auto'>
+                            <div className='dl'>
+                              <select className='custom-select'>
+                                <option value='0' selected=''>
+                                  Monthly
+                                </option>
+                                <option value='1'>Daily</option>
+                                <option value='2'>Weekly</option>
+                                <option value='3'>Yearly</option>
+                              </select>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='table-responsive'>
+                        <table className='table v-middle'>
+                          <thead>
+                            <tr className='bg-light'>
+                              <th className='border-top-0'>Name</th>
+                              <th className='border-top-0'>Ticket Issues</th>
+                              <th className='border-top-0'>Priority</th>
+                              <th className='border-top-0'>Status</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {tickets.map((ticket) => (
+                              <TicketList key={ticket.id} ticket={ticket} />
+                            ))}
+                          </tbody>
+                        </table>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              {/* vhjgjhd */}
+                {/* vhjgjhd */}
+              </div>
             </div>
           </div>
         </div>
+        <div></div>
+        {/* footer */}
+        <Footer />
       </div>
-      <div></div>
-      {/* footer */}
-      <Footer />
     </div>
-  </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => ({
   ticket: state.ticket,
 });
 
-export default connect(mapStateToProps, {getTickets})(TicketPage) ;
+export default connect(mapStateToProps, { getTickets })(TicketPage);
