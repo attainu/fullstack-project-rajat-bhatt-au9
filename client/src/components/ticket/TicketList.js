f2bd010b7034187b84ad9a700134759983037f0b
shubham-pal-au9 <spal5776@gmail.com>
2021-03-15 21:21:45 +0530

    Added ticket listing page

---
+++ client/src/components/ticket/TicketList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+ const TicketList = ({ticket}) => {
+    return (
+         
+    <tr>
+        <td>
+          <div className='d-flex align-items-center'>
+            <div className='m-r-10'>
+              <img className='btn btn-circle text-white' src={ticket.avatar} />
+            </div>
+            <div className=''>
+              <h4 className='m-b-0 font-16'>{ticket.name}</h4>
+            </div>
+          </div>
+        </td>
+        <td>{ticket.ticket_issue}</td>
+        <td>{ticket.priority}</td>
+        <td>
+          <label className='label label-danger'>{ticket.status}</label>
+        </td>
+        
+    </tr>
+    )
+}
+
+export default TicketList;
