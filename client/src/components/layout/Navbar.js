90d02a214ff32c43a87daa093ed3b08ddac4458b
shubham-pal-au9 <spal5776@gmail.com>
2021-03-16 20:01:46 +0530

    Updated navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,52 +3,69 @@ import { Link } from "react-router-dom";
 import { logout } from "../../actions/auth";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import "./navbar.css";
 
-const Navbar = ({ auth: { isAuthenticated, loading ,user}, logout }) => (
-  <Fragment>
-    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-      <Link className='navbar-brand' to='/admin-dashboard'>
-        CRM Ticket System
-      </Link>
-      <button
-        className='navbar-toggler'
-        type='button'
-        data-toggle='collapse'
-        data-target='#navbarSupportedContent'
-        aria-controls='navbarSupportedContent'
-        aria-expanded='false'
-        aria-label='Toggle navigation'
-      >
-        <span className='navbar-toggler-icon'></span>
-      </button>
-      <div className='collapse navbar-collapse ' id='navbarSupportedContent'>
-        <ul className='nav navbar-nav'>
-          <li className='nav-item active'>
-          {user.role === 'Admin' ? 
-          <Link className='nav-link' to='/admin-dashboard'>Dashboard</Link>
-          : <Link className='nav-link' to='/client-dashboard'>Dashboard</Link>}
-            
-          </li>
-          <li className='nav-item'>
-            <Link className='nav-link' to='/user-page'>
-            {user.role === 'Admin' ? <span> Client </span> : null }
-            </Link>
-          </li>
-          <li className='nav-item'>
+const Navbar = ({ auth: { isAuthenticated, loading ,user}, logout }) =>{
+
+  const client_links = (
+    <nav class="navbar bg-dark">
+    
+      <p><Link to='/admin-dashboard'><i class="fas fa-user"></i> CRM Ticket System</Link></p>
+    <ul>
+    <li>
+      <Link className='nav-link' to='/client-dashboard'>Dashboard</Link>
+        </li>
+        <li>
             <Link className='nav-link' to='/ticket'>
               Ticket
             </Link>
           </li>
-          <li className='nav-item'>
-            <a className='nav-link' onClick={logout} href='#!'>
-              Logout
-            </a>
-          </li>
-        </ul>
-      </div>
+          <li>
+            <Link className='nav-link' to='#' onClick={logout}><i class="fas fa-sign-out-alt"></i>
+             Logout
+            </Link>
+        </li>
+    </ul>
     </nav>
+  );
+
+  const admin_links =(
+    <nav class="navbar bg-dark">
+    
+      <p><Link to='/client-dashboard'><i class="fas fa-user"></i> CRM Ticket System</Link></p>
+    <ul>
+  <li>
+  <Link className='nav-link' to='/admin-dashboard'>  <i className="fas fa-user"></i> <span >Dashboards</span> </Link>
+  </li>
+  <li>
+            <Link className='nav-link' to='/user-page'>
+        <span> Client </span>
+            </Link>
+        </li>
+        
+       <li>
+            <Link className='nav-link' to='/ticket'>
+              Ticket
+            </Link>
+          </li>
+
+         <li>
+            <Link className='nav-link' to='#' onClick={logout}><i class="fas fa-sign-out-alt"></i>
+             Logout
+            </Link>
+        </li>
+    </ul>
+    </nav>
+  );
+
+  return (
+    <Fragment>
+
+      {user.role ==="Admin" ? admin_links : client_links}
+      
   </Fragment>
-);
+  )
+} 
 
 Navbar.propsTypes = {
   logout: PropTypes.func.isRequired,
